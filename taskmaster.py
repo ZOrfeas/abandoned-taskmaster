4dd59eb8adf1b3848b993939045745b0ac5c6454
provlhmatikos <orfeas.zografos@gmail.com>
2020-10-11 18:44:05 +0300

    today() and day() appear to be complete. week() and crontab intergration remaing

--- taskmaster.py
+++ taskmaster.py
@@ -20,6 +20,17 @@ longOptions = {
     "week"   :5,
     "day"   :6
 }
+class color:
+   purple = '\033[95m'
+   cyan = '\033[96m'
+   darkcyan = '\033[36m'
+   blue = '\033[94m'
+   green = '\033[92m'
+   yellow = '\033[93m'
+   red = '\033[91m'
+   bold = '\033[1m'
+   underline = '\033[4m'
+   end = '\033[0m'
 
 def longShortPicker(argument):
     if (len(argument) > 2) and (argument[1] == '-'):
@@ -31,6 +42,38 @@ def longShortPicker(argument):
 
 
 class Taskmaster:
+        
+    def printRecurring(recurringTask,nowDay):
+        print(color.red+recurringTask["name"]+color.end)
+        timesList = []
+        for i in recurringTask["daysTimes"]:
+            if i[0]==nowDay and len(i)==2:
+                timesList.append(i[1])
+        print(color.bold+color.underline+"When:"+color.end)
+        if timesList:        
+            timesList = sorted(timesList)
+            print("At ", end='')
+            print(*timesList, sep=", ")
+        else:
+            print("Anytime")
+        if recurringTask["description"] is not None:
+            print(color.bold+color.underline+"Info:"+color.end)
+            print(recurringTask["description"])
+
+    def printOneOff(oneOffTask):
+        print(color.red+oneOffTask["name"]+color.end)
+        if oneOffTask["isDeadlined"]: print(color.blue+"!!!DEADLINE!!!"+color.end)
+        print(color.bold+color.underline+"When:"+color.end)
+        if oneOffTask["time"] is None:
+            print("Anytime")
+        elif oneOffTask["time"] in ['morning','noon','afternoon','evening','night']:
+            print("In the "+oneOffTask["time"]) if oneOffTask["time"] in ["morning","afternoon","evening"] else print("At"+oneOffTask["time"])
+        else:
+            print("At "+ oneOffTask["time"])
+        if oneOffTask["description"] is not None:
+            print(color.bold+color.underline+"Info:"+color.end)
+            print(oneOffTask["description"])
+
 
     def help():
         print(
@@ -59,7 +102,7 @@ class Taskmaster:
             else: helper = False
         
         def recurringCreate(name):
-            daysTimes = input("Specify when the task shall repeat separated by commas[e.g.(case insensitive) Mon|Tue|Wed|Thu|Fri|Sat|Sun 14:15, Tue 12:45...]: \n")
+            daysTimes = input("Specify when the task shall repeat separated by commas[e.g. Mon|Tue|Wed|Thu|Fri|Sat|Sun 14:15, Tue 12:45...(time can be ommited)]: \n")
             #splits daysTimes seperated by commas after lower-casing them, strips whitespace then splits days and times apart in two piece tuples
             daysTimes = list(map(lambda x:x.split(), list(map(lambda x:x.strip(), daysTimes.lower().split(',')))))
             description = input("Explain the task in more detail if you want to: \n")
@@ -92,9 +135,9 @@ class Taskmaster:
                     dateHelper = False
             timeHelper = True
             while(timeHelper):
-                time = input("Is it time specific? If so specify[Hour:Min(24-hour format) or morning|noon|afternoon|evening|nighttime], can also be left empty: \n")
+                time = input("Is it time specific? If so specify[Hour:Min(24-hour format) or morning|noon|afternoon|evening|night], can also be left empty: \n")
                 time = time.lower()
-                if (time not in ['morning','noon','afternoon','evening','nighttime','']):
+                if (time not in ['morning','noon','afternoon','evening','night','']):
                     try:
                         datetime.datetime.strptime(time,"%H:%M")
                     except ValueError:
@@ -128,6 +171,7 @@ class Taskmaster:
                     isDone = True
             sys.stdout.write(line)
         print("Task successfully created")
+    
     def remove():
         helper = True
         while(helper):
@@ -141,23 +185,97 @@ class Taskmaster:
         os.system(command)
         print("Task successfully deleted")
 
+                    
     def today():
         dateObject = datetime.date.today()
         todaysDate = datetime.datetime.strftime(dateObject, "%d-%m-%Y")
         todaysDay = datetime.datetime.strftime(dateObject,"%a").lower()
+
         grepPatternRecurring = '"'+todaysDay+'"'
         grepPatternOneOffs = '"'+todaysDate+'"'
+
         processRecurring = subprocess.run(['grep',grepPatternRecurring,home+"/.tasksFile"],stdout=subprocess.PIPE,universal_newlines=True)
         processOneOffs = subprocess.run(['grep',grepPatternOneOffs,home+"/.tasksFile"],stdout=subprocess.PIPE,universal_newlines=True)
+
         todaysRecurring = processRecurring.stdout.splitlines()
         todaysOneOffs = processOneOffs.stdout.splitlines()
-        # print(todaysRecurring)
-        # print(todaysOneOffs)
+        todaysRecurring = list(map(lambda x:json.loads(x),todaysRecurring))
+        todaysOneOffs = list(map(lambda x:json.loads(x),todaysOneOffs))
+
+        
+        print('')
+        print("                 Today you need to                 ")
+        print('')
+        if todaysRecurring:
+            for i in todaysRecurring:
+                Taskmaster.printRecurring(i,todaysDay)
+                print("___________________________________________________")
+        if todaysOneOffs:
+            for i in todaysOneOffs:
+                Taskmaster.printOneOff(i)
+                print("___________________________________________________")
+        if not todaysOneOffs and not todaysRecurring:
+            print("                  Do nothing !!(?)                 ")
+        else:
+            print('')
+            print("                    Good luck...                 ")
+        print('')
 
     def week():
         print("mphka week")
     def day():
-        print("mphka day")
+        dateHelper=True
+        while(dateHelper):
+            date = input("Which day's schedule?[in DD-MM-YYYY format, 'today','tomorrow','the day after' also work as well as numbers counting up from today(0)]: \n")
+            if date == 'today' or date == '0':
+                Taskmaster.today()
+                exit()
+            if(date not in['today','tomorrow','the day after']) and not date.isnumeric():
+                try:
+                    targetDateObject = datetime.datetime.strptime(date,"%d-%m-%Y")
+                except ValueError:
+                    print("Invalid input, try again")
+                else: dateHelper = False
+            else:
+                if date == 'today': date = 0
+                elif date == 'tomorrow': date = 1
+                elif date == 'the day after': date = 2
+                today = datetime.date.today()
+                targetDateObject = today + datetime.timedelta(days=date)
+                dateHelper = False
+        targetDate = datetime.datetime.strftime(targetDateObject,"%d-%m-%Y")
+        targetDay = datetime.datetime.strftime(targetDateObject,"%a").lower()
+        grepPatternRecurring = '"'+targetDay+'"'
+        grepPatternOneOffs = '"'+targetDate+'"'
+
+        processRecurring = subprocess.run(['grep',grepPatternRecurring,home+"/.tasksFile"],stdout=subprocess.PIPE,universal_newlines=True)
+        processOneOffs = subprocess.run(['grep',grepPatternOneOffs,home+"/.tasksFile"],stdout=subprocess.PIPE,universal_newlines=True)
+
+        targetRecurring = processRecurring.stdout.splitlines()
+        targetOneOffs = processOneOffs.stdout.splitlines()
+        targetRecurring = list(map(lambda x:json.loads(x),targetRecurring))
+        targetOneOffs = list(map(lambda x:json.loads(x),targetOneOffs))
+        print('')
+        if date == 1:
+            print("                Tomorrow you need to                ")
+        else:
+            print("             At "+targetDate+" you need to             ")
+        print('')
+        if targetRecurring:
+            for i in targetRecurring:
+                Taskmaster.printRecurring(i,targetDay)
+                print("___________________________________________________")
+        if targetOneOffs:
+            for i in targetOneOffs:
+                Taskmaster.printOneOff(i)
+                print("___________________________________________________")
+        if not targetOneOffs and not targetRecurring:
+            print("                  Do nothing !!(?)                 ")
+        else:
+            print('')
+            print("                    That's all...                 ")  
+        print('') 
+
 
     responseFuncs = {
         1:help,
