cd996dec008a46f6147d34691b9f33216cd62e7d
provlhmatikos <orfeas.zografos@gmail.com>
2020-10-12 23:31:12 +0300

    wrongInputMessage and then whatever.

--- taskmaster_v1.py
+++ taskmaster_v1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys,os
+import sys,os,json
 from pathlib import Path
 
 
@@ -10,12 +10,27 @@ class Taskmaster:
     recurringTasksFile = tasksFolderLocation+"/recurringTasks"
     oneOffTasksFile = tasksFolderLocation+"/oneOffTasks"
     confFile = tasksFolderLocation+"/conf"
+    
+    options = {}
 
     class FileWriter:
-        dummy = "dummy"
+        
+        @staticmethod
+        def initConfFile():
+            with open(Taskmaster.confFile,'w') as optionsFile:
+                optionsFile.write('{}\n')
+    
     class FileReader:
-        dummy = "dummy"
-    class Printer:
+        
+        @staticmethod
+        def fetchConfOptions():
+            with open(Taskmaster.confFile) as optionsFile:
+                fetchedOptions = json.loads(optionsFile.read().strip())
+                return fetchedOptions
+        
+    
+    class outputWriter:
+        
         color = {
             "purple": '\033[95m',
             "cyan": '\033[96m',
@@ -28,9 +43,49 @@ class Taskmaster:
             "underline": '\033[4m',
             "end": '\033[0m'
         }
+        @classmethod
+        def helpMessage(cls):
+            title =       '                                       TASKMASTER                                       '
+            explanation = '                          A cli app to help organize your days                          '
+            usage =       'Usage: taskmaster.py [OPTION] [FURTHER ARGS]                                            '
+            tip =         'All options can be given FURTHER ARGS but will exit if input is invalid:                '
+            command0 =    '  -h, --help                 outputs this help message and exit\n'
+            command1 =    '  -c, --create               creates a task, if no FURTHER ARGS are given prompts       \n'
+            more1    =    '                             accordingly, use "taskmaster.py -c help" for more info     \n'
+            command2 =    '  -s, --say, -p              say/print a day\'s, week\'s schedule or all recurring tasks\n'
+            more2    =    '                             currently registered\n'
+            command3 =    '  -d, --delete               delete a task by name\n'
+            command4 =    '  -o, --options, --conf      prompts to change the given option, or all if not specified'
+            splittingLine='----------------------------------------------------------------------------------------'
+            examples=     'Examples:'
+            print('')
+            print(cls.color["bold"]+title+cls.color["end"])
+            print(cls.color["bold"]+splittingLine+cls.color["end"])
+            print(explanation)
+            print('')
+            print(usage)
+            print(tip)
+            print(command0+command1+command1+more1+command2+more2+command3+command4)
+            print(cls.color["bold"]+splittingLine+cls.color["end"])
+            print(examples)
+            print(cls.color["bold"]+splittingLine+cls.color["end"])
+            print('Me: Orfeas Zografos')
+            print('orfeas.zografos@gmail.com for if you find I f\'ed something up too much')
+            print('or to tell me how cool this is(\'nt)')
+
+        @classmethod
+        def wrongInputMessage(cls,invalidInput):
+
+    class inputReader:
+        
+        dummy = "dummy"
+
     @classmethod
-    def help(cls,inputOK=True):
-        print("Entered help")
+    def help(cls,invalidInput=''):
+        if not invalidInput:
+            cls.outputWriter.helpMessage()
+        else:
+            cls.outputWriter.wrongInputMessage(invalidInput)
     @classmethod
     def createTask(cls,creatorArguments):
         print("Entered createTask")
@@ -42,41 +97,49 @@ class Taskmaster:
         print("Entered deleteTask")
     @classmethod
     def configure(cls,confArgumets):
-        print("Entered configure")
-    
+        if os.stat(cls.confFile).st_size == 0:
+            cls.FileWriter.initConfFile()
+        
+            
+
     @classmethod
     def parseArgsAndDecide(cls,argumentsList):
         if len(argumentsList)==0 or argumentsList[0] in ['-h','--help']:
-            Taskmaster.help()
+            cls.help()
             return 0
         elif argumentsList[0] in ['-c','--create']:
-            return Taskmaster.createTask(argumentsList[1:])
+            return cls.createTask(argumentsList[1:])
         elif argumentsList[0] in ['-s','--say','-p']:
-            return Taskmaster.printSchedule(argumentsList[1:])
+            return cls.printSchedule(argumentsList[1:])
         elif argumentsList[0] in ['-d','--delete']:
-            return Taskmaster.deleteTask(argumentsList[1:])
-        elif argumentsList[0] in ['-o','--configure','--options']:
-            return Taskmaster.configure(argumentsList[1:])
+            return cls.deleteTask(argumentsList[1:])
+        elif argumentsList[0] in ['-o','--conf','--options']:
+            return cls.configure(argumentsList[1:])
         else:
-            Taskmaster.help(False)
+            cls.help(argumentsList[0])
             return 1
     
     @classmethod
+    def setConfOptions(cls):
+        cls.options = cls.FileReader.fetchConfOptions()
+    
+    @classmethod
     def createOrVerifyDir(cls):
-        if not os.path.exists(Taskmaster.tasksFolderLocation):
-            os.makedirs(Taskmaster.tasksFolderLocation)
-        if not os.path.exists(Taskmaster.recurringTasksFile):
-            open(Taskmaster.recurringTasksFile, 'w').close()
-        if not os.path.exists(Taskmaster.oneOffTasksFile):
-            open(Taskmaster.oneOffTasksFile, 'w').close()
-        if not os.path.exists(Taskmaster.confFile):
-            open(Taskmaster.confFile, 'w').close()
-            Taskmaster.configure([])
+        if not os.path.exists(cls.tasksFolderLocation):
+            os.makedirs(cls.tasksFolderLocation)
+        if not os.path.exists(cls.recurringTasksFile):
+            open(cls.recurringTasksFile, 'w').close()
+        if not os.path.exists(cls.oneOffTasksFile):
+            open(cls.oneOffTasksFile, 'w').close()
+        if not os.path.exists(cls.confFile):
+            open(cls.confFile, 'w').close()
+            cls.configure([])
 
-    @staticmethod
-    def do(argumentsList):
-        Taskmaster.createOrVerifyDir()
-        return Taskmaster.parseArgsAndDecide(argumentsList)
+    @classmethod
+    def do(cls,argumentsList):
+        cls.createOrVerifyDir()
+        cls.setConfOptions()
+        return cls.parseArgsAndDecide(argumentsList)
 
 
 def main(argvector):
